8d1597331b69b07ed9b0ce13a489716fe1d07620
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-09-16 06:14:39 +0100

    added db user and started the connection process

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+mongoose.connect();
 // console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on Port ${port}`);
 });
+
+// shell connection string mongo "mongodb+srv://cluster0.zzmg1.mongodb.net/<dbname>" --username monarene
